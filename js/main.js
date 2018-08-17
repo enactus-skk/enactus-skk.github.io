9322553e234493ca98649594aa3e0698596d02fb
intiwatana <merryyear45@gmail.com>
2018-08-17 14:14:17 +0900

    slider test

--- js/main.js
+++ js/main.js
@@ -36,3 +36,36 @@
 		}, 500);
 	});
 })();
+
+$(document).ready(function () {
+		var $tabs = $('#horizontalTab');
+		$tabs.responsiveTabs({
+				rotate: false,
+				startCollapsed: 'accordion',
+				collapsible: 'accordion',
+				setHash: true,
+				/*animation: 'slide',*/
+				disabled: [],
+				click: function(e, tab) {
+						// $('.info').html('Tab <strong>' + tab.id + '</strong> clicked!');
+				},
+				activate: function(e, tab) {
+						// $('.info').html('Tab <strong>' + tab.id + '</strong> activated!');
+				},
+				activateState: function(e, state) {
+						//console.log(state);
+						// $('.info').html('Switched from <strong>' + state.oldState + '</strong> state to <strong>' + state.newState + '</strong> state!');
+				}
+		});
+
+		$('#enable-tab').on('click', function() {
+				$tabs.responsiveTabs('enable', 3);
+		});
+		$('#disable-tab').on('click', function() {
+				$tabs.responsiveTabs('disable', 3);
+		});
+		$('.select-tab').on('click', function() {
+				$tabs.responsiveTabs('activate', $(this).val());
+		});
+
+});
